bbc533d2f4ba554a0bfafcbb19e2e5b67e1c8c81
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-20 14:40:46 -0700

    exercise 19 completed

--- exercises/ex19.js
+++ exercises/ex19.js
@@ -4,3 +4,21 @@ Call the emotions() function so that it prints the output you see below, but ins
 emotions("happy", laugh(2)); // you can use your laugh function from the previous quizzes
 Prints: "I am happy, haha!"
 */
+
+//Is this the function declaration? not inline function expression?
+// const myEmotion = function emotions(emotion, laugh){
+//     console.log (`I am ${emotion}, ${laugh}!`)
+// }
+// const laugh = function(num){
+//     return ("ha".repeat(num));
+// }
+// myEmotion("happy", laugh(2));
+
+//after i googled, i found out that the above was wrong.
+function emotions(emotion, callback){
+    console.log(`I am ${emotion}, ${callback(2)}!`);
+}
+
+emotions("happy", function(num){
+    return "ha".repeat(num);
+});
