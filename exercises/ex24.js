d2f5889b6ac11964f621c4ab50dc52f07d747920
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-20 21:00:57 -0700

    exercise 24 completed

--- exercises/ex24.js
+++ exercises/ex24.js
@@ -20,3 +20,14 @@ console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
 console.log(lastIndexOf([], 3), "=?", -1);
 
 */
+
+const lastIndexOf = function(array, value){
+    for (let i = array.length - 1; i >= 0; i--){
+        if (array[i] === value){
+            return i;
+        }
+    }
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
